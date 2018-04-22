a70a0a73c51bf1d9dde73d77cb9e1640d8958a5a
theGreenJedi <GreenJedi@protonmail.com>
2018-04-22 08:35:57 -0400

    chapter1

---
+++ chapter1/helloworld.py
@@ -0,0 +1,18 @@
+#stupid hello world program
+#
+print('Hello World!')
+#
+# Ask for their name
+#
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' +myName)
+print('The length of your name is: ')
+print (len(myName))
+#
+#ask for their age
+#
+print('What is your age?')
+myAge = input()
+print('You will be ' +str(int(myAge) ' in a year from now')
+      
